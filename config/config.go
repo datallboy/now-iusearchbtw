3031a48bbb4d46118d1e931950ebe8bf33c04418
Datallboy <github@datallboy.com>
2023-07-19 10:20:56 -0400

    Add config options

--- config/config.go
+++ config/config.go
@@ -6,9 +6,14 @@ import (
 	"github.com/docker/docker/client"
 )
 
+var PUBLIC_PATH = "./public"
+var LISTENING_ADDRESS = "0.0.0.0"
+
 type Config struct {
-	Client  *client.Client
-	Context context.Context
+	Client           *client.Client
+	Context          context.Context
+	PublicPath       string
+	ListeningAddress string
 }
 
 func New() (*Config, error) {
@@ -20,7 +25,9 @@ func New() (*Config, error) {
 	ctx := context.Background()
 
 	return &Config{
-		Client:  cli,
-		Context: ctx,
+		Client:           cli,
+		Context:          ctx,
+		PublicPath:       PUBLIC_PATH,
+		ListeningAddress: LISTENING_ADDRESS,
 	}, nil
 }
