25255f6db59148769c5c5865184a81e5376e78d5
Datallboy <github@datallboy.com>
2023-07-26 12:08:43 -0400

    v1.1 - Fix resize of tty and xtermjs

--- src/index.js
+++ src/index.js
@@ -2,31 +2,33 @@ import { Terminal } from 'xterm';
 import { FitAddon } from 'xterm-addon-fit';
 import { AttachAddon } from 'xterm-addon-attach';
 import 'xterm/css/xterm.css';
+import './index.css';
+
+let socket;
+var resizedId;
+
+const term = new Terminal();
+const fitAddon = new FitAddon();
+term.loadAddon(fitAddon);
 
 document.addEventListener('DOMContentLoaded', function () {
   fetch('/new')
     .then((response) => response.json())
     .then((json) => {
-      const term = new Terminal();
       const { containerID } = json.data;
-      console.log(`Connecting to container: ${containerID}`);
-      const socket = new WebSocket(
+      socket = new WebSocket(
         `ws://localhost:2376/containers/${containerID}/attach/ws?stream=true&stdin=true&stdout=true&stderr=true`
       );
       const attachAddon = new AttachAddon(socket, { bidirectional: true });
 
-      const fitAddon = new FitAddon();
-
       term.loadAddon(attachAddon);
-      term.loadAddon(fitAddon);
 
       term.open(document.getElementById('xterm-container'));
 
       fitAddon.fit();
 
       socket.onopen = (e) => {
-        console.log('Connection established');
-        socket.send('\u000A');
+        resizeTty(term);
       };
 
       socket.onclose = (e) => {
@@ -34,6 +36,20 @@ document.addEventListener('DOMContentLoaded', function () {
       };
     });
 });
+
+window.addEventListener('resize', function () {
+  clearTimeout(resizedId);
+  resizedId = setTimeout(resizeTty, 300);
+});
+
+const resizeTty = () => {
+  if (term && fitAddon) {
+    fitAddon.fit();
+    socket.send(`stty columns ${term.cols} rows ${term.rows}`);
+    socket.send('\u000A');
+    socket.send('\u000C');
+  }
+};
 
 const killContainer = (containerID) => {
   fetch(`/kill?containerID=${containerID}`, { method: 'DELETE' }).then(() =>
